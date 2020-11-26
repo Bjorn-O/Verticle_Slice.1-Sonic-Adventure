72f1ba4271b20ae70bc297e751acd11038d817b7
Björn <Bjorn.oudkerk@outlook.com>
2020-11-26 10:43:17 +0100

    Added class structure for trackers and Level Objects

---
+++ Sonic Adventure VS/Assets/Scripts/LevelObjects/CheckPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : LevelObject_BASE
+{
+    protected override void OnTriggerEnter(Collider player)
+    {
+        base.OnTriggerEnter(player);
+
+        if (isPlayer)
+        {
+            SetCheckPoint();
+        }
+    }
+
+    void SetCheckPoint()
+    {
+        
+    }
+}
---
+++ Sonic Adventure VS/Assets/Scripts/LevelObjects/LevelObject_BASE.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelObject_BASE : MonoBehaviour
+{
+    public Rigidbody rb;
+    protected bool isPlayer;
+
+
+    protected virtual void OnTriggerEnter(Collider player)
+    {
+        if (player.tag == "Player")
+        {
+            Debug.Log(player.name + " Passed through " + this.name);
+            isPlayer = true;
+        }
+    }
+}
