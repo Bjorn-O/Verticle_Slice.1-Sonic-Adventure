a4b8e8a6065907b975d0dd598bbc8d0f4eea1c1a
Björn <Bjorn.oudkerk@outlook.com>
2020-11-30 12:21:52 +0100

    Added some smaller changes to the gameplay system

--- Sonic Adventure VS/Assets/Scripts/GameTrackers/LevelTracker.cs
+++ Sonic Adventure VS/Assets/Scripts/GameTrackers/LevelTracker.cs
@@ -7,6 +7,8 @@ namespace GameManager
     public class LevelTracker : MonoBehaviour
     {
         [SerializeField] CheckPoint _lastCheckPoint;
+        [SerializeField] GameObject _playerCharacter;
+        [SerializeField] Transform _startLocation;
         public CheckPoint lastCheckPoint
         {
             get {
@@ -14,19 +16,42 @@ namespace GameManager
             }
             set {
                 _lastCheckPoint = value;
+                _checkPointTime = _time;
+                //Make the _checkPointTime flash in the UI
             }
         }
         
-        [SerializeField] GameObject _playerCharacter;
-
-        
-        public string timeText;
+        private string _timeText;
+        public string timeText{
+            get{
+                return _timeText;
+            }
+        }
         float _time;
+        float _checkPointTime;
 
+        void Start()
+        {
+            _playerCharacter = Instantiate(_playerCharacter, _startLocation.position, Quaternion.identity);
+            //Transition screen
+            //Give player control of Sonic
+
+        }
         void Update()
         {
             _time += Time.deltaTime;
             TimeFormatter(_time);            
+        }
+
+        void Respawn()
+        {
+            // Take control away from _playerCharacter
+            // Transition screen to black
+            // Turn off the timer
+            // Set the time to _checkPointTime
+            // Fade the screen back in
+            // Give player Control of _playerCharacter
+            // Start the timer
         }
 
         void TimeFormatter(float fTime)
@@ -35,10 +60,8 @@ namespace GameManager
             int minutes = intTime / 60;
             int seconds = intTime % 60;
             float fraction = fTime * 1000;
-            // Debug.Log(fraction);
             fraction = fraction % 1000;
-            // Debug.Log(fraction);
-            timeText = string.Format ("{0:00}:{1:00}:{2:000}", minutes, seconds, fraction);
+            _timeText = string.Format ("{0:00}:{1:00}:{2:000}", minutes, seconds, fraction);
         }
     }
 }
--- Sonic Adventure VS/Assets/Scripts/GameTrackers/SonicTracker.cs
+++ Sonic Adventure VS/Assets/Scripts/GameTrackers/SonicTracker.cs
@@ -6,19 +6,86 @@ namespace GameManager
 {
     public class SonicTracker : MonoBehaviour
     {
+        [SerializeField] AudioSource[] _soundEffects;
+        [SerializeField] GameObject _droppedRing;
         private int _ringCount;
         public int ringCount{
             get{
                 return _ringCount;
             }
             set{
-                _ringCount += 1;
-                if (_ringCount >= 100)
+                if (value < 0)
                 {
-                    _lifeCount ++;
+                    DropRings(_ringCount);
+                }
+                _ringCount += value;
+                _ringsToExtraLife += value;
+            }
+        }
+        private int _ringsToExtraLife{
+            get{
+                return _ringsToExtraLife;
+            }
+            set{
+                _ringsToExtraLife += value;
+                if (_ringsToExtraLife >= 100)
+                {
+                    lifeCount++;
+                    _ringsToExtraLife -= 100;
+                } else if (_ringsToExtraLife < 0)
+                {
+                    _ringsToExtraLife = 0;
                 }
             }
         }
         private int _lifeCount;
+        public int lifeCount{
+            get{
+                return _lifeCount;
+            }
+            set{
+                switch (value)
+                {
+                    case 1:
+                        _lifeCount += value;
+                        _soundEffects[0].Play();
+                        break;
+                    case -1:
+                        if (_lifeCount == 0)
+                        {
+                            // Call onto LevelTracker.GameOver();
+                        }
+                        _lifeCount += value;
+                        break;
+                    default:
+                    break;
+                }
+            }
+        }
+
+        void GetHurt()
+        {
+            if (ringCount > 0)
+            {
+                DropRings(ringCount);
+            }
+            else
+            {
+                PlayerDeath();
+            }
+        }
+
+        void DropRings(int ringsDropped)
+        {
+            // Check if the amount of rings is above 20
+            // If not, calculate 360/ringcount to get the angle of firing in a circle.
+            // Instantiate the amount of rings each in their own direction
+            // Note to self, how do I instantly instantiate each ring in a different direction with force? Maybe put force in the awake function but that won't help the direction.
+        }
+
+        void PlayerDeath()
+        {
+            // Call onto LevelTracker.Respawn
+        }
     }
 }
